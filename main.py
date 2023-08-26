b5a36bd2c2c5295c5f0eab3fa18af278498b81a8
eriosta <84827743+eriosta@users.noreply.github.com>
2023-08-26 23:50:53 +0000

    Header

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ nav = st.sidebar.radio("Navigation", ["Get Started","Data", "Exploratory Data An
 
 if nav == "Get Started":
     st.markdown("""
-    🚀 **Welcome to MEDML - A No-Code AI Platform For Clinicians**
+    🚀 **# Welcome to MEDML! A No-Code AI Platform For Clinicians.**
 
     Whether you're a tech guru or a complete beginner, MEDML is designed with you in mind. We're breaking barriers, ensuring that everyone, regardless of their background in tech, AI, or data, can harness the power of machine learning in medicine.
 
