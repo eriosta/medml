dda3180e22084c5d202e22a1a875187539888cd4
eriosta <84827743+eriosta@users.noreply.github.com>
2023-08-27 21:01:49 +0000

    Imports

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pandas as pd
 import os
 from models import prepare_data, train_and_evaluate_models, plot_evaluation_metrics, get_model_hyperparameters, LogisticRegression, RandomForestClassifier, XGBClassifier, DecisionTreeClassifier
 from learn import show
-from chat import llama2
+from chat import llama2, pandas_ai
 
 st.sidebar.title("MEDML")
 
