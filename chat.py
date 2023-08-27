9d27294a3989bdd58c6c63c377351ac7d3465608
eriosta <84827743+eriosta@users.noreply.github.com>
2023-08-27 13:57:09 +0000

    API token instructions

--- chat.py
+++ chat.py
@@ -5,8 +5,7 @@ import random
 
 suggested_questions = [
         "How does this chatbot work?",
-        "How can I get my own Replicate API key?",
-        "What's the difference between a dataset and a database?",
+        "Explain dataset vs. database?",
         "Can you explain the basics of AI and ML and DL?",
         "What are data cleaning best practices?",
         "What is supervised vs unsupervised learning?",
@@ -41,7 +40,7 @@ def llama2():
         top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
         max_length = st.sidebar.slider('max_length', min_value=64, max_value=4096, value=512, step=8)
         
-        st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
+        st.markdown('Need a Replicate API token? Get it [here](https://replicate.com/account/api-tokens)!')
     os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
     # Store LLM generated responses
