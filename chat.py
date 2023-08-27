a30acc4eee683a4efc960524fb2cecac4050177d
eriosta <84827743+eriosta@users.noreply.github.com>
2023-08-27 21:22:05 +0000

    Added StreamlitMiddleware class

--- chat.py
+++ chat.py
@@ -4,8 +4,11 @@ import os
 import random
 import pandas as pd
 from pandasai import PandasAI
-from pandasai.llm.openai import OpenAI
 import matplotlib.pyplot as plt
+
+from pandasai import PandasAI, SmartDataframe
+from pandasai.llm.openai import OpenAI
+from pandasai.middlewares import StreamlitMiddleware
 
 suggested_questions = [
         "How does this chatbot work?",
@@ -119,13 +122,14 @@ def pandas_ai():
 
     if "openai_key" in st.session_state:
         if st.session_state.df is None:
+            st.text("Go to Data to upload your data source.")
             uploaded_file = st.file_uploader(
                 "Choose a CSV file. This should be in long format (one datapoint per row).",
                 type="csv",
             )
             if uploaded_file is not None:
-                df = pd.read_csv(uploaded_file)
-                st.session_state.df = df
+                # Use SmartDataframe and include the StreamlitMiddleware
+                st.session_state.df = SmartDataframe(uploaded_file, {"middlewares": [StreamlitMiddleware()]})
 
         with st.form("Question"):
             question = st.text_input("Question", value="", type="default")
@@ -148,7 +152,6 @@ def pandas_ai():
 
         st.subheader("Prompt history:")
         st.write(st.session_state.prompt_history)
-
 
     if st.button("Clear"):
         st.session_state.prompt_history = []
