4bc0495a3cb512fe25c03c3fdbdd4552a3cc76d6
eriosta <84827743+eriosta@users.noreply.github.com>
2023-08-29 07:37:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -101,7 +101,10 @@ class ModelsPage:
     def sub_navigation(self):
         model_page = st.sidebar.radio("Navigate", ['Train & Evaluate', 'Explain'])
         if model_page == 'Train & Evaluate':
-            train()
+            if df in st.session_state:
+                train()
+            else:
+                st.warning("Please load data source under **Data** first")
         elif model_page == 'Explain':
             st.warning("Under construction.")
 
